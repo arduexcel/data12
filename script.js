2123572510f98aeb60de0cd2a43f9d2e8dedd8c2
arduexcel <arduexcel1@gmail.com>
2026-04-20 03:51:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function printFinalReport() {
     // encodeURIComponent بەکار دێت بۆ ئەوەی ئەگەر ناوەکە کوردی بێت تێک نەچێت
     window.open(`report.html?user=${encodeURIComponent(currentUser)}`, '_blank');
 }
+<script>
+    // ... کۆدە کۆنەکانی تر وەک خۆی ...
+
+    window.onload = initReport;
+
+    // ئەم بەشە نوێیە بۆ داخستنەوەی وێبەکە دوای پرنت
+    window.onafterprint = function() {
+        window.close();
+    };
+</script>
